853c94d2d96010b253b7263d1b52918d07e6b53a
Bky <buckcode16@gmail.com>
2026-05-24 15:14:56 +0900

    Add: lru-cache - submission-8

---
+++ Data Structures & Algorithms/lru-cache/submission-8.py
@@ -0,0 +1,51 @@
+class ListNode:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+
+        self.next = self.prev = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.cap = capacity
+
+        self.left, self.right = ListNode(0,0), ListNode(0,0)
+        self.left.next, self.right.prev = self.right, self.left
+        
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+
+        self.remove(self.cache[key])
+        self.insert(self.cache[key])
+
+        return self.cache[key].val
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+
+        self.cache[key] = ListNode(key,value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+
+            # could also use self.cache.pop(lru.key), 
+            # del is a better choice for the fail-fast no safety check purpose
+            del self.cache[lru.key]
+        
+    
+    def insert(self, node):
+        prv,nxt = self.right.prev, self.right
+        prv.next = nxt.prev = node
+        node.prev, node.next = prv, nxt
+    
+    def remove(self, node):
+        prv, nxt = node.prev, node.next
+        prv.next = nxt
+        nxt.prev = prv
