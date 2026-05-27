cbaa8beb460bb53952b9a5257be4248f8eff1647
Bky <buckcode16@gmail.com>
2026-05-27 18:15:24 +0900

    Add: lru-cache - submission-10

---
+++ Data Structures & Algorithms/lru-cache/submission-10.py
@@ -0,0 +1,45 @@
+class ListNode:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}
+
+        self.left , self.right = ListNode(0,0), ListNode(0,0)
+        self.left.next, self.right.prev = self.right, self.left
+        
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+
+        self.remove(self.cache[key])
+        self.insert(self.cache[key])
+
+        return self.cache[key].val
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+
+        self.cache[key] = ListNode(key,value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+        
+    def insert(self, node):
+        # insert to right
+        prv, nxt = self.right.prev, self.right
+        prv.next = nxt.prev = node
+        node.next, node.prev = nxt, prv
+
+    def remove(self, node):
+        prv, nxt = node.prev, node.next
+        prv.next, nxt.prev = nxt, prv
